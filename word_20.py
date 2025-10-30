3b6c1a06144519faf6168e485b7cb831693848c7
Nibedita Nayak <nibeditanayak474@gmail.com>
2025-10-30 19:46:00 +0530

    Update input prompt and word extraction logic
    Refactor input prompt and improve word printing logic.

--- word_20.py
+++ word_20.py
@@ -1,1 +1,11 @@
-name=input("enter anything you want to print")
+sentence=input("enter anything you want to print:")
+word=""
+for char in sentence:
+    if char!=" ":
+     word=word+char
+    else:
+       if word!="":
+          print(word)
+          word=""
+if word!="":
+       print(word)    
