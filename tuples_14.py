fa2cc5f31ebf22cef3b3d5325e03c7ea09ea1c9e
Nibedita Nayak <nibeditanayak474@gmail.com>
2025-10-30 19:52:44 +0530

    Add tuple operations for fruits and nesting

--- tuples_14.py
+++ tuples_14.py
@@ -1,1 +1,24 @@
-tup =[]
+fruits = ("apple", "banana", "cherry", "mango")
+
+print("Tuple of fruits:", fruits)
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+print("First two fruits:", fruits[:2])
+
+print("All fruits:")
+for fruit in fruits:
+    print(fruit)
+
+if "banana" in fruits:
+    print("Yes, banana is in the tuple!")
+
+print("Number of fruits:", len(fruits))
+
+more_fruits = ("orange", "grape")
+all_fruits = fruits + more_fruits
+print("Combined tuple:", all_fruits)
+
+nested = (fruits, more_fruits)
+print("Nested tuple:", nested)
+
+
