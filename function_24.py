a5cfdedd304a0b5248b509e29ea7ca69220c2d8c
Nibedita Nayak <nibeditanayak474@gmail.com>
2025-11-01 20:39:05 +0530

    Add files via upload

---
+++ function_24.py
@@ -0,0 +1,11 @@
+def greet():
+    print("hello,welcome to my code!")
+greet()
+def exam(a,b):
+    result=a+b
+    print("result is:",result)
+exam(2,3)
+def mult(c,d):
+    return c*d
+answer=mult(12,3)
+print("multiplication  is:",answer)
