9de1b0152c3cbdc3b2e112e8c72c7ecfa77084de
Nibedita Nayak <nibeditanayak474@gmail.com>
2025-11-01 20:26:30 +0530

    Add files via upload

---
+++ continue_23.py
@@ -0,0 +1,17 @@
+count = 0
+while(count <= 15):
+    if count == 5:
+        count+=count
+        continue
+    print(count)
+    count += 1
+for i in range(10):
+    if i==3:
+        continue
+    print(i)
+num=1
+for i in range(10):
+ if i==3:
+    continue
+ print(10*i)
+ num+=1
