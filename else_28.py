6dfd504cc173a4bc05b3c534c734a1fc803af194
Nibedita Nayak <nibeditanayak474@gmail.com>
2025-11-04 22:48:19 +0530

    Add files via upload

---
+++ else_28.py
@@ -0,0 +1,16 @@
+for i in range(5):
+ print(i)
+else:
+ print("out of loop")
+for x in range(5):
+ print("itration no{}in for loop".format(x+1))
+else:
+ print("else block in loop")
+ print("out of loop")
+count=0
+while count<=3:
+ print("count:",count)
+ count +=1
+else:
+ print("loop finished successfully")
+ 
