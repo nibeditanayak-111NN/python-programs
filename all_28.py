9eb5e6e83d5f638b41d3784a10a5be6aac73392f
Nibedita Nayak <nibeditanayak474@gmail.com>
2026-02-09 20:25:59 +0530

    Add files via upload

---
+++ all_28.py
@@ -0,0 +1,15 @@
+print("nibedita nayak")
+input("please enter your name :")
+int(input("please enter your age:"))
+name ="nibedita"
+print(name)
+print(type(name))
+a = 3
+b = 4
+print("c=",a+b)
+print("c=",a-b)
+print("c=",a*b)
+print("c=",a%b)
+print(a==b)
+print(a<=b)
+print(a>=b)
