e786f2f0929a1ac865d8cac0385476fea5aa77f8
Nibedita Nayak <nibeditanayak474@gmail.com>
2026-02-14 18:09:25 +0530

    Duplicate code and add student dictionary

--- all_28.py
+++ all_28.py
@@ -13,3 +13,17 @@ print("c=",a%b)
 print(a==b)
 print(a<=b)
 print(a>=b)
+marks = [10,11,12,13,14,15,16,17,18,19,20]
+print(marks)
+print(marks[2:6])
+student ={
+    "name":"nibedita",
+    "age": 22,
+    "class": 90,
+    "roll": 10,
+    "marks":95
+}
+print(student)
+print(student.get("name"))
+print(student.get("roll"))
+print(student.get("std"))
