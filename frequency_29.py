781d8b4b0ad7d6e59b9d5dd9dc5bb3d8b0afe4cf
Nibedita Nayak <nibeditanayak474@gmail.com>
2026-02-25 09:13:40 +0530

    Refactor frequency counting logic in frequency_29.py

--- frequency_29.py
+++ frequency_29.py
@@ -6,3 +6,13 @@ for num in set(numbers):
     count =numbers.count(num)
     if count > 2:
         print(num, "appears", count, "times")
+numbers = [4,7,2,7,8,2,4,4,9,8,7,1]
+set(numbers)
+
+count = 1
+for num in set(numbers):
+    count =numbers.count(num)
+    if count > 2:
+        print(num, "appears", count, "times")
+
+
